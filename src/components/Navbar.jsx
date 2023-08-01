2504a469bd2ab4878ee88be892174dd3178f2dfb
asharkhann2 <asharpunk02@gmail.com>
2023-08-01 20:02:12 +0500

    duplicate about us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,6 +29,7 @@ function Navbar() {
             <li className='p-4 border-b  border-gray-600' >Company</li>
             <li className='p-4 border-b  border-gray-600' >Resources</li>
             <li className='p-4 border-b border-gray-600'>About</li>
+            <li className='p-4 border-b border-gray-600'>About</li>
             <li className='p-4'>Contact</li>
 
             </ul>
